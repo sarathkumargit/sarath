80673e94a519e4265c2296a76657a3c1c9b4eba9
k.sarathkumar <166106731+sarathkumargit@users.noreply.github.com>
2025-11-28 14:16:08 +0530

    Update logo image size in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
           <a href="#home" className="flex items-center space-x-2 group">
             
              
-              <img src={logo} alt="Logo" className="w-15 h-15" />
+              <img src={logo} alt="Logo" className="w-8 h-8" />
           
             
           </a>
